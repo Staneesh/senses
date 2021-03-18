bb9769e8edb5824f4d34632c70ebf0f6f206dfc0
Staneesh <blkreasons@outlook.com>
2021-03-18 13:09:03 +0100

    Updating window dimensions

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
 extern crate sdl2;
 extern crate gl;
 
+use std::convert::TryInto;
+
 fn main() {
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
+
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(3, 3);
+
+    let (mut window_width, mut window_height) = (900, 700);
     let window = video_subsystem
-        .window("senses", 900, 700)
+        .window("senses", window_width, window_height)
         .opengl()
         .resizable()
         .build()
         .unwrap();
+
+
     let _gl_context = window.gl_create_context().unwrap();
     let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
     
     //TODO(stanisz): remove this unsafe block
     unsafe {
+        gl::Viewport(0, 0, window_width.try_into().unwrap(), window_height.try_into().unwrap());
         gl::ClearColor(0.3, 0.3, 0.5, 1.0);
     }
 
@@ -26,6 +37,15 @@ fn main() {
                 _ => {},
             }
         }
+
+        {
+            let wsz = window.size();
+            window_width = wsz.0;
+            window_height = wsz.1;
+            
+            println!("{}, {}", window_width, window_height);
+        }
+
 
         unsafe {
             gl::Clear(gl::COLOR_BUFFER_BIT);
