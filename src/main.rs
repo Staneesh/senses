d4c066612ac1321c656b72f9f584da7b4403a1a3
Staneesh <blkreasons@outlook.com>
2021-03-18 12:42:42 +0100

    Unsafe window coloring

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,11 @@ fn main() {
     let _gl_context = window.gl_create_context().unwrap();
     let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
     
+    //TODO(stanisz): remove this unsafe block
+    unsafe {
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
+
     let mut event_pump = sdl.event_pump().unwrap();
     'game_loop: loop { 
         for event in event_pump.poll_iter() {
@@ -20,6 +25,10 @@ fn main() {
                 sdl2::event::Event::Quit {..} => break 'game_loop,
                 _ => {},
             }
+        }
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
         }
 
         window.gl_swap_window();
