29d5f035e95692631e3cda6be28e66a7eecbb084
Staneesh <blkreasons@outlook.com>
2021-03-18 12:18:42 +0100

    SDL2 crate imported and window created

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
+extern crate sdl2;
 extern crate gl;
 
 fn main() {
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let _window = video_subsystem
+        .window("senses", 900, 700)
+        .resizable()
+        .build()
+        .unwrap();
+    
+    let mut event_pump = sdl.event_pump().unwrap();
+    
+    'game_loop: loop { 
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit {..} => break 'game_loop,
+                _ => {},
+            }
+        }
+    }
 }
